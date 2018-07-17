2d30e1023ea23df0b96c6d93b443946d3fc09961
nathan-08 <nathanryan001@gmail.com>
2018-07-17 11:59:38 -0600

    test component for displaying book thumbnails

--- src/components/testComponent/testComponent.jsx
+++ src/components/testComponent/testComponent.jsx
@@ -1,8 +1,66 @@
 import React from 'react'
+import Cover from '../../assets/img/generic-cover.jpg';
 
 export default props => (
     <div className="test-component">
-        test compoennt
+        <span>test component</span>
+        <div className="grid-container">
+            <div className="book-thumbnail">
+                <img src={Cover}/>
+                <div className="left-side">
+                    <div className="book-title">Book Title</div> 
+                    <div className="author-name"><em>by</em> Author Name</div>
+                    <div className="buy-button">BUY</div>
+                </div>
+            
+            </div>
+            <div className="book-thumbnail">
+                <img src={Cover}/>
+                <div className="left-side">
+                    <div className="book-title">Book Title</div> 
+                    <div className="author-name"><em>by</em> Author Name</div>
+                    <div className="buy-button">BUY</div>
+                </div>
+            
+            </div>
+            <div className="book-thumbnail">
+                <img src={Cover}/>
+                <div className="left-side">
+                    <div className="book-title">Book Title</div> 
+                    <div className="author-name"><em>by</em> Author Name</div>
+                    <div className="buy-button">BUY</div>
+                </div>
+            
+            </div>
+            <div className="book-thumbnail">
+                <img src={Cover}/>
+                <div className="left-side">
+                    <div className="book-title">Book Title</div> 
+                    <div className="author-name"><em>by</em> Author Name</div>
+                    <div className="buy-button">BUY</div>
+                </div>
+            
+            </div>
+            <div className="book-thumbnail">
+                <img src={Cover}/>
+                <div className="left-side">
+                    <div className="book-title">Book Title</div> 
+                    <div className="author-name"><em>by</em> Author Name</div>
+                    <div className="buy-button">BUY</div>
+                </div>
+            
+            </div>
+            <div className="book-thumbnail">
+                <img src={Cover}/>
+                <div className="left-side">
+                    <div className="book-title">Book Title</div> 
+                    <div className="author-name"><em>by</em> Author Name</div>
+                    <div className="buy-button">BUY</div>
+                </div>
+            
+            </div>
+
+        </div>
 
     </div>
 )
