825cb1b40708c15f2327122064157304ce792e4a
nathan-08 <nathanryan001@gmail.com>
2018-07-17 15:35:05 -0600

    modal

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,36 @@ import Modal from './components/Modal/Modal';
 
 class App extends Component {
 
+  constructor(){
+    super()
+    this.state={
+      modalTitle: '',
+      modalText: ''
+    }
+  }
+
   componentDidMount(){
     setTimeout(function(){
       document.body.classList.add('is-mounted')
     },1000*.3)
   }
+  
+  callModal = (title, text) => {
+    this.setState({
+      modalTitle: title,
+      modalText: text
+    }, ()=>{
+      document.body.classList.toggle('show-modal')
+    })
+  }
   render() {
     return (
       <div className="App">
-      <Modal/>
+      <Modal title={this.state.modalTitle} text={this.state.modalText}/>
       <Opacity/>
         <Nav/>
         {routes}
+        <button onClick={_=>this.callModal('heythere', 'buddy')}>click meh okay</button>
       </div>
     );
   }
