6896436868d2993b9883a70b81a74d2d91bd855e
nathan-08 <nathanryan001@gmail.com>
2018-08-05 14:13:31 -0600

    added files

---
+++ src/components/Landing/Landing.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import family from '../../assets/img/family_medium1.jpg'
+
+export default function (props) {
+
+    return ( 
+        <div className="landing-component">
+        <div className="background-image"></div>
+            <div className="text-container">
+                    <h1>A Secular or a Gospel Approach?</h1>
+                    <p>The principles of the gospel, when understood and practiced by both spouses, have the greatest chance of helping Latter-day Saint couples build strong, stable marriages, the kind of marriages that inspire both partners to value their relationship and to develop a companionship that creates in both a desire to be ‘together forever.’</p>
+                    <p style={{textAlign:"right"}}>from the book, <em>Marital Therapy</em> by Doug Brinley</p>
+            </div>
+
+        </div>
+    )
+}
--- src/routes.jsx
+++ src/routes.jsx
@@ -4,15 +4,17 @@ import Home from './components/Home/home';
 import About from './components/About/About';
 import TestComponent from './components/testComponent/testComponent';
 import Cart from './components/Cart/Cart';
+import Landing from './components/Landing/Landing'
+import Books from './components/Books/Books'
 
 export default (
     <Switch>
-        <Route exact path="/" component={Home}/>
+        <Route exact path="/" component={Landing}/>
         <Route path="/about" component={About}/>
         <Route path="/cart" component={Cart}/>
         
         
-        <Route path="/other-stuff" component={TestComponent}/>
+        <Route path="/other-stuff" component={Books}/>
     </Switch>
 )
 
