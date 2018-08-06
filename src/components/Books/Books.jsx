0ac9deae7658925b1eae2fa22eecbf4b37977050
nathan-08 <nathanryan001@gmail.com>
2018-08-06 00:41:29 -0600

    fixed image popin

--- src/components/Books/Books.jsx
+++ src/components/Books/Books.jsx
@@ -1,88 +1,127 @@
 import React from "react";
-import Button from '../Button/Button'
+import Button from "../Button/Button";
+import marriageBand from "../../assets/img/marriage-band-1920-1080.jpg";
+import brideGroom from "../../assets/img/bride-groom1620-1080.jpg";
 
-export default props => {
-  return (
-    <div className="books-component">
-      <BookDisplay
-        title="Marital Therapy"
-        text={
-          <div>
-            <p>
-              Marriage between a man and a woman has been the divine pattern originating in the Garden of Eden with our
-              First Parents <a href='https://www.lds.org/scriptures/ot/gen/1.26-27' target='blank'>(Genesis 1:26-27)</a>, and this union has been the traditional form of marriage for millennia.
-              The First Couple were married by God before death entered the world. Therefore marriage was intended to be
-              an eternal relationship. It is made possible through the resurrection brought about by Jesus Christ. A
-              priesthood key restored by Elijah seals a couple together for ‘time and for eternity.’
-            </p>
-            <p>
-              Of course all marriages have interesting challenges that surface over the life cycle. There are basically
-              two approaches to assist Latter-day Saint couples in their quest for marital happiness: (1) a gospel, or
-              doctrinal-based perspective, and (2) a secular or non-religious approach, the foundation of professional
-              counseling protocols.
-            </p>
-            <p>
-              The point of this volume: Why not try a gospel-based approach to resolve marital issues? The principles of
-              the gospel, when understood and practiced by both spouses, have the greatest chance of helping Latter-day
-              Saint couples build strong, stable marriages, the kind of marriages that inspire both partners to value
-              their relationship and to develop a companionship that creates in both a desire to be ‘together forever.’
-            </p>
-            <p>
-              During his ministry, President Boyd K. Packer of the Quorum of the Twelve Apostles was fond of stating
-              that doctrine was a powerful tool in changing and improving behavior. “True doctrine, understood,” he
-              said, “will change behavior quicker than will the study of behavior change behavior’ <a href='https://www.lds.org/general-conference/2004/04/do-not-fear?lang=eng' target='blank'>(“Do Not Fear,”
-              Ensign, May 2004, 73.)</a> This book attests to that principle.
-            </p>
-          </div>
-        }
-      />
-      <BookDisplay
-        title="What We Wish We'd Known Before Our Honeymoon"
-        text={
-          <div>
-              
-            <p>
-              Marriage is the great adventure of mortality. We rejoice as parents, siblings, and friends when we receive
-              an invitation to a reception of someone we know and love. Having been worthy to marry in a Church temple,
-              surely they anticipate with some enthusiasm, at the end of their courtship and now sealing, to begin a
-              life of sexual intimacy that binds their hearts and souls together.
-            </p>
-            <p>
-              However, heading off on a honeymoon to a private location, a common tradition for LDS couples, requires a
-              little planning to find a place to spend a few days of private time. Some opt for an ocean cruise to
-              Mexican resorts or to Alaska, some opt for California and the theme parks there. Others head for the
-              beauty of Yellowstone, Glacier National Park, or even Banff, Canada, and the stunning sites in the land to
-              the north. The idea is to get away from family and friends in a secluded place where they can make plans
-              for their immediate future. An important part of the honeymoon period will be learning from each other the
-              intricacies of physical intimacy.
-            </p>
-            <p>
-              Though highly anticipated by these couples, sexual relations are often more difficult for them than
-              portrayed by actors in films. In fact, as you will see, many couples and the first few nights of their
-              time together rather frustrating as they learn that what they thought would be simple and pleasurable, may
-              be difficult. However, the joy of being together, of exploring male/female attributes now as legal
-              companions, they now must learn from each other about such things as arousal, passion, and intercourse as
-              important elements of their new companionship. This book is designed to help newlyweds achieve success in
-              the intimate exchanges they share ‘between husband and wife.’
-            </p>
-          </div>
-        }
-      />
-    </div>
-  );
-};
+export default class Books extends React.Component {
+  componentDidMount() {
+    const bgImg1 = new Image();
+    const bgImg2 = new Image();
+    bgImg1.src = marriageBand;
+    bgImg2.src = brideGroom;
+
+    const imagesLoaded = new Promise(function(resolve, reject) {});
+
+    let bg1Loaded = false;
+    let bg2Loaded = false;
+    console.time("book-imgs-load");
+    bgImg1.onload = function() {
+      bg1Loaded = true;
+      if (bg2Loaded) {
+        document.querySelector(".books-component").classList.add("fadein");
+        console.timeEnd("book-imgs-load");
+      }
+    };
+    bgImg2.onload = function() {
+      bg2Loaded = true;
+      if (bg1Loaded) {
+        document.querySelector(".books-component").classList.add("fadein");
+        console.timeEnd("book-imgs-load");
+      }
+    };
+    const bgImageElements = document.querySelectorAll(".bg-image");
+    bgImageElements[0].style.backgroundImage = "url(" + bgImg1 + ")";
+    bgImageElements[1].style.backgroundImage = "url(" + bgImg2 + ")";
+  }
+  render() {
+    return (
+      <div className="books-component">
+        <BookDisplay
+          title="Marital Therapy"
+          text={
+            <div>
+              <p>
+                Marriage between a man and a woman has been the divine pattern originating in the Garden of Eden with
+                our First Parents{" "}
+                <a href="https://www.lds.org/scriptures/ot/gen/1.26-27" target="blank">
+                  (Genesis 1:26-27)
+                </a>, and this union has been the traditional form of marriage for millennia. The First Couple were
+                married by God before death entered the world. Therefore marriage was intended to be an eternal
+                relationship. It is made possible through the resurrection brought about by Jesus Christ. A priesthood
+                key restored by Elijah seals a couple together for ‘time and for eternity.’
+              </p>
+              <p>
+                Of course all marriages have interesting challenges that surface over the life cycle. There are
+                basically two approaches to assist Latter-day Saint couples in their quest for marital happiness: (1) a
+                gospel, or doctrinal-based perspective, and (2) a secular or non-religious approach, the foundation of
+                professional counseling protocols.
+              </p>
+              <p>
+                The point of this volume: Why not try a gospel-based approach to resolve marital issues? The principles
+                of the gospel, when understood and practiced by both spouses, have the greatest chance of helping
+                Latter-day Saint couples build strong, stable marriages, the kind of marriages that inspire both
+                partners to value their relationship and to develop a companionship that creates in both a desire to be
+                ‘together forever.’
+              </p>
+              <p>
+                During his ministry, President Boyd K. Packer of the Quorum of the Twelve Apostles was fond of stating
+                that doctrine was a powerful tool in changing and improving behavior. “True doctrine, understood,” he
+                said, “will change behavior quicker than will the study of behavior change behavior’{" "}
+                <a href="https://www.lds.org/general-conference/2004/04/do-not-fear?lang=eng" target="blank">
+                  (“Do Not Fear,” Ensign, May 2004, 73.)
+                </a>{" "}
+                This book attests to that principle.
+              </p>
+            </div>
+          }
+        />
+        <BookDisplay
+          title="What We Wish We'd Known Before Our Honeymoon"
+          text={
+            <div>
+              <p>
+                Marriage is the great adventure of mortality. We rejoice as parents, siblings, and friends when we
+                receive an invitation to a reception of someone we know and love. Having been worthy to marry in a
+                Church temple, surely they anticipate with some enthusiasm, at the end of their courtship and now
+                sealing, to begin a life of sexual intimacy that binds their hearts and souls together.
+              </p>
+              <p>
+                However, heading off on a honeymoon to a private location, a common tradition for LDS couples, requires
+                a little planning to find a place to spend a few days of private time. Some opt for an ocean cruise to
+                Mexican resorts or to Alaska, some opt for California and the theme parks there. Others head for the
+                beauty of Yellowstone, Glacier National Park, or even Banff, Canada, and the stunning sites in the land
+                to the north. The idea is to get away from family and friends in a secluded place where they can make
+                plans for their immediate future. An important part of the honeymoon period will be learning from each
+                other the intricacies of physical intimacy.
+              </p>
+              <p>
+                Though highly anticipated by these couples, sexual relations are often more difficult for them than
+                portrayed by actors in films. In fact, as you will see, many couples and the first few nights of their
+                time together rather frustrating as they learn that what they thought would be simple and pleasurable,
+                may be difficult. However, the joy of being together, of exploring male/female attributes now as legal
+                companions, they now must learn from each other about such things as arousal, passion, and intercourse
+                as important elements of their new companionship. This book is designed to help newlyweds achieve
+                success in the intimate exchanges they share ‘between husband and wife.’
+              </p>
+            </div>
+          }
+        />
+      </div>
+    );
+  }
+}
 
 function BookDisplay(props) {
   return (
     <section>
-        <div className="bg-image"></div>
-        <div className="book-section-container">
-          <div className="text-container">
-              <h1>{props.title}</h1>
-              <Button text='add to cart'/>
-              {props.text}
-          </div>
+      <div className="bg-image" />
+      <div className="book-section-container">
+        <div className="text-container">
+          <h1>{props.title}</h1>
+          <Button text="add to cart" />
+          {props.text}
         </div>
+      </div>
     </section>
   );
 }
--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,17 +1,41 @@
-import React from 'react'
-import family from '../../assets/img/family_medium1.jpg'
+import React from "react";
+import family from "../../assets/img/family_medium1.jpg";
 
-export default function (props) {
+export default class Landing extends React.Component {
+  componentDidMount() {
+    var bgImage = new Image();
+    bgImage.onprogress = function(e) {
+        console.log(`
+            __progress__
+        `, e)
+}
+    console.time('load image')
+    bgImage.onload = function() {
+        console.timeEnd('load image')
+        document.querySelector('.landing-component').classList.add('fadein')
+    };
+    document.querySelector(".background-image").style.backgroundImage = "url(" + family + ")";
+    bgImage.src = family
 
-    return ( 
-        <div className="landing-component">
-        <div className="background-image"></div>
-            <div className="text-container">
-                    <h1>A Secular or a Gospel Approach?</h1>
-                    <p>The principles of the gospel, when understood and practiced by both spouses, have the greatest chance of helping Latter-day Saint couples build strong, stable marriages, the kind of marriages that inspire both partners to value their relationship and to develop a companionship that creates in both a desire to be ‘together forever.’</p>
-                    <p style={{textAlign:"right"}}>from the book, <em>Marital Therapy</em> by Doug Brinley</p>
-            </div>
+}
 
+  render() {
+    return (
+      <div className="landing-component">
+        <div className="background-image" />
+        <div className="text-container">
+          <h1>A Secular or a Gospel Approach?</h1>
+          <p>
+            The principles of the gospel, when understood and practiced by both spouses, have the greatest chance of
+            helping Latter-day Saint couples build strong, stable marriages, the kind of marriages that inspire both
+            partners to value their relationship and to develop a companionship that creates in both a desire to be
+            ‘together forever.’
+          </p>
+          <p style={{ textAlign: "right" }}>
+            from the book, <em>Marital Therapy</em> by Doug Brinley
+          </p>
         </div>
-    )
+      </div>
+    );
+  }
 }
