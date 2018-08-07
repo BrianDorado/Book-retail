67da83bce6cd969c432e48119607b978f29597dd
nathan-08 <nathanryan001@gmail.com>
2018-08-07 16:40:45 -0600

    footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default props => {
+  return (
+    <div className="footer-component">
+      <div className="footer-information">
+        <span>hello</span> | <span>other thing</span> | 
+        <a href="https://www.github.com" target='blank'> github <i className="fab fa-github" /></a>
+      </div>
+    </div>
+  );
+};
