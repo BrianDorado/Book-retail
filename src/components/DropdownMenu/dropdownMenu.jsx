1a769c9229d9f10f50904c2009f205c5605d25f6
nathan-08 <nathanryan001@gmail.com>
2018-08-08 10:17:17 -0600

    commit

--- src/components/DropdownMenu/dropdownMenu.jsx
+++ src/components/DropdownMenu/dropdownMenu.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default props => {
     return (
         <div id="drop-down-menu">
-            <h1>Contact</h1><br/>
-            <h1>Home</h1><br/>
-            <h1>Books</h1><br/>
-            <h1>About</h1><br/>
+            <Link to='/'><h1>Contact</h1></Link><br/>
+            <Link to='/other-stuff'><h1>Home</h1></Link><br/>
+            <Link to='/'><h1>Books</h1></Link><br/>
+            <Link to='/'><h1>About</h1></Link><br/>
 
 
         </div>
